3ddf754e49f0568feeda3ed4a1917eec91343395
salohiddin <salohiddinurinboev1203@gmail.com>
2023-06-01 14:29:21 +0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.talabalarstependiyasi {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.talabalarstependiyasi to javafx.fxml;
+    exports com.example.talabalarstependiyasi;
+}
